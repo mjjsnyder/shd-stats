bc4fb3991c79770876f992c3b9b2e511d35358c7
mjjsnyder <mjjsnyder@gmail.com>
2013-10-05 19:21:22 -0700

    Clean up non-essential code and comments now that history is available in GitHub

--- urlib2scrape.py
+++ urlib2scrape.py
@@ -11,10 +11,11 @@ headers = {
   'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:10.0.1) Gecko/20100101 Firefox/10.0.1',
 }
 opener.addheaders = headers.items()
-url = 'YOU MIGHT WANT TO ADD SOMETHING HERE, KNUCKLEHEAD!!!'
+url = 'http://statsheet.com/mcb/games/2013/03/15/unlv-75-colorado-state-65/play_by_play'
 getWebsite = opener.open(url)
 pageContent = getWebsite.read()
 soup = BeautifulSoup(pageContent)
+
 #should have the full source of the page at this point
 
 #identify this tag on the page: <table border="0" style="font-size:.9em" cellpadding="2" cellspacing="0">
@@ -22,34 +23,21 @@ table = soup.find('table', border="0", style="font-size:.9em", cellpadding="2")
 
 #NOTE to put into readable format: table.prettify())
 
-#first event in the table
-#print(table.next.next.nextSibling.nextSibling.prettify())
-
-#Number of events in the game no longer needed with curated allEvents Object
-#identify this tag for events: <tr onmouseover="hl(this)" onmouseout="uhl(this)" style="">
-#totalNumEvents = len(table.find_all("tr", onmouseover="hl(this)", onmouseout="uhl(this)", style=""))
 allEvents = table.find_all("tr", onmouseover="hl(this)", onmouseout="uhl(this)", style="")
-
 
 #obtain team names
 homeTeam = table.next.next.next.nextSibling.nextSibling.nextSibling.string
 awayTeam = table.next.next.next.nextSibling.string
 
-#game score at first event in table
-#firstEventAwayScore = table.next.next.nextSibling.nextSibling.next.nextSibling.nextSibling.string
-#firstEventHomeScore = table.next.next.nextSibling.nextSibling.next.nextSibling.nextSibling.nextSibling.nextSibling.string
-
 print('GATHERED FROM %s' % (url))
 print('The home team is %s and the away team is %s.' % (homeTeam, awayTeam))
-#print('There are %d events in this game.' % (totalEvents))
-#print('The first event occured with %s remaining on the game clock.' % (firstEventTime[:-2]))
-#print('Score: %s - %s | %s - %s' % (awayTeam, firstEventAwayScore, homeTeam, firstEventHomeScore))
 
 tempEvent = ''
 eventTime = ''
 eventTeam = ''
 eventAction = ''
 eventPlayer = ''
+#set scores to ridiculous number so that leaving them unchanged will be immediately apparent in stat analysis
 homeScore = 10000
 awayScore = 10000
 count = 0
