c05dd52f86aa3fa39ce2295f511b87d515a6607f
mjjsnyder <mjjsnyder@gmail.com>
2013-10-05 20:46:28 -0700

    Added functionality for tracking score through play-by-play. Added error handling for when timeouts are called. More cleanup.

--- urlib2scrape.py
+++ urlib2scrape.py
@@ -1,13 +1,14 @@
 from bs4 import BeautifulSoup
 import urllib2
+
+#Initialize variables
 tempEvent = ''
 eventTime = ''
 eventTeam = ''
 eventAction = ''
 eventPlayer = ''
-#Initialize scores to ridiculous number so that leaving them unchanged will be immediately apparent in stat analysis
-homeScore = '10000'
-awayScore = '10000'
+homeScore = 0
+awayScore = 0
 count = 0
 
 #NOTE to put soup into readable format: table.prettify())
@@ -20,7 +21,7 @@ headers = {
   'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:10.0.1) Gecko/20100101 Firefox/10.0.1',
 }
 opener.addheaders = headers.items()
-url = 'http://statsheet.com/mcb/games/2013/03/15/unlv-75-colorado-state-65/play_by_play'
+url = raw_input('URL to scrape:')
 getWebsite = opener.open(url)
 pageContent = getWebsite.read()
 soup = BeautifulSoup(pageContent)
@@ -46,23 +47,34 @@ for i in allEvents:
     #Grab the game clock for the individual event
     eventTime = i.next.string
 
-    #scoring is done in a weird way. will have to add logic to the eventAction object to control score
-    #homeScore = i.next.nextSibling.nextSibling.string
-    #awayScore = i.next.nextSibling.nextSibling.nextSibling.nextSibling.string
-
     #Determine which team the event is relevant to
-    if i.next.nextSibling.next.string == None:
-        eventTeam = homeTeam
-        eventPlayer = i.find('a').string
-        eventAction = i.find('span').string
-    else:
-        eventTeam = awayTeam
-        eventPlayer = i.find('a').string
-        eventAction = i.find('span').string
+    try:
+        if i.next.nextSibling.next.string == None:
+            eventTeam = homeTeam
+            eventPlayer = i.find('a').string
+            eventAction = i.find('span').string
+            if 'made' in eventAction:
+                if 'free throw' in eventAction:
+                    homeScore += 1
+                if ('2-point' in eventAction) or ('dunk' in eventAction) or ('layup' in eventAction):
+                    homeScore += 2
+                if '3-point' in eventAction:
+                    homeScore += 3
+        else:
+            eventTeam = awayTeam
+            eventPlayer = i.find('a').string
+            eventAction = i.find('span').string
+            if 'made' in eventAction:
+                if 'free throw' in eventAction:
+                    awayScore += 1
+                if ('2-point' in eventAction) or ('dunk' in eventAction) or ('layup' in eventAction):
+                    awayScore += 2
+                if '3-point' in eventAction:
+                    awayScore += 3
 
-    #for when I fix the score tracking
-    #print('Time = %s, Team = %s, Player = %s, Action = %s, Home Score = %s, Away Score = %s' % (eventTime[:-2], eventTeam, eventPlayer, eventAction, homeScore, awayScore))
-    print('Time = %s, Team = %s, Player = %s, Action = %s' % (eventTime[:-2], eventTeam, eventPlayer, eventAction))
-    count += 1
-    if count == 2:
-        break
+        print('Time = %s, Team = %s, Player = %s, Action = %s, Home Score = %s, Away Score = %s' % (eventTime[:-2], eventTeam, eventPlayer, eventAction, homeScore, awayScore))
+        count += 1
+        if count == 50:
+            break
+    except:
+        print ('TIMEOUT')
